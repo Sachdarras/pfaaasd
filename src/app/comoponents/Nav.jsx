ced8d4eafb8701109996c98a0045eeebc39a8812
Sacha Darras <sachdarras@gmail.com>
2024-10-09 10:54:53 +0200

    menu burguer

--- src/app/comoponents/Nav.jsx
+++ src/app/comoponents/Nav.jsx
@@ -1,7 +1,7 @@
-// src/components/Nav.jsx
- "use client"
+"use client";
 import Link from 'next/link';
 import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Importer les icônes pour le menu
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,31 +10,39 @@ function Nav() {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false); // Ferme le menu
+  };
+
   return (
-    <div>
+    <nav>
+      {/* Menu burger visible uniquement sur mobile et tablette */}
       <div className="burger" onClick={toggleMenu}>
-        <div></div>
-        <div></div>
-        <div></div>
+        {isOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-      <ul className={`nav-list ${isOpen ? 'active' : ''}`}>
-        <li>
-          <Link href="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/projet" className="nav-link">
-            Projet
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" className="nav-link">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </div>
+
+      {/* La fenêtre de navigation qui s'ouvre sur mobile/tablette */}
+      <div className={`nav-modal ${isOpen ? 'active' : ''}`}>
+        <button className="close-button" onClick={closeMenu}>✖</button> {/* Bouton de fermeture */}
+        <ul className="nav-list">
+          <li>
+            <Link href="/" className="nav-link" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/projet" className="nav-link" onClick={closeMenu}>
+              Projet
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="nav-link" onClick={closeMenu}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
