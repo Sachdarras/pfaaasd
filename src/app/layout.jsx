b2c175aa94a08f0095701ff23a85e43853b9380e
Sacha Darras <sachdarras@gmail.com>
2024-10-09 12:03:28 +0200

    favion

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,23 @@
 import Image from 'next/image';
-import Nav from '../../src/app/comoponents/Nav'
-import Footer from '../../src/app/comoponents/Footer';
-import "./style/global.scss"
+import Nav from '../app/comoponents/Nav'; // Corrige le chemin ici
+import Footer from '../app/comoponents/Footer'; // Corrige le chemin ici
+import Head from 'next/head'; // Assurez-vous d'importer Head pour les métadonnées
+import './style/global.scss';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <Head>
+        <link rel="icon" href="/favicon.icoS" />
+        {/* Ajoutez d'autres liens ou métadonnées ici si nécessaire */}
+      </Head>
       <body>
-    <div className="header-container ">
-        <Image src="/assets/logo/logowithoutBG.png" className="logoasd" alt="Logo" width={150} height={150} />
+        <div className="header-container">
+          <Image src="/assets/logo/logowithoutBG.png" className="logoasd" alt="Logo" width={150} height={150} />
         </div>
-        <Nav/>
-        
-        {children} 
-         <Footer/>
+        <Nav />
+        {children}
+        <Footer />
       </body>
     </html>
   );
