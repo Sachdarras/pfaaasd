63972a7322ce84547e6a45b1265638909c933287
Sacha Darras <sachdarras@gmail.com>
2024-10-25 12:18:00 +0200

    secret clic

--- src/app/components/Description.jsx
+++ src/app/components/Description.jsx
@@ -2,15 +2,27 @@
 
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation'; // Importez useRouter
 
 function Description() {
   const [descriptions, setDescriptions] = useState([]);
+  const [clickCount, setClickCount] = useState(0); // État pour suivre les clics
+  const router = useRouter(); // Utilisez useRouter pour la redirection
 
   useEffect(() => {
     fetch('/api/description')
       .then((res) => res.json())
       .then((data) => setDescriptions(data));
   }, []);
+
+  const handleProfileClick = () => {
+    setClickCount((prevCount) => prevCount + 1); // Incrémente le compteur
+
+    if (clickCount + 1 === 3) { // Vérifie si le compteur atteint 3
+      router.push('/auth/signin'); // Redirige vers la page de connexion
+      setClickCount(0); // Réinitialise le compteur après redirection
+    }
+  };
 
   return (
     <div className="content">
@@ -26,12 +38,14 @@ function Description() {
         ))}
       </div>
       <Image 
-        src={descriptions.length > 0 ? descriptions[0].image : '/assets/images/profil.jpg'} // Assurez-vous que le chemin est correct ou récupéré dynamiquement
+        src={descriptions.length > 0 ? descriptions[0].image : '/assets/images/profil.jpg'} // Vérifiez que le chemin est correct
         className="profil" 
         alt="profil" 
         width={150} 
         height={150} 
+        onClick={handleProfileClick} // Ajoutez le gestionnaire d'événements
       />
+     
     </div>
   );
 }
