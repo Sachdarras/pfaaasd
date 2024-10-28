1d9b9db58c58467dfd3f886444970b7a33b5fdc5
Sacha Darras <sachdarras@gmail.com>
2024-10-28 22:06:33 +0100

    peau blanche

--- src/app/admin/projet/page.jsx
+++ src/app/admin/projet/page.jsx
@@ -2,38 +2,39 @@
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Link from 'next/link';
+import Link from 'next/link'; 
 import { useRouter } from 'next/navigation';
-import Cookies from 'js-cookie';
-import Image from 'next/image';
+import Cookies from 'js-cookie'; 
+import Image from 'next/image'; 
 
 const AdminProjects = () => {
   const [projects, setProjects] = useState([]);
-  const [skillsList, setSkillsList] = useState([]);
+  const [skillsList, setSkillsList] = useState([]); 
   const [formData, setFormData] = useState({
     name: '',
-    img: null, // Remplacer l'URL par null pour un fichier local
+    img: '',
     description: '',
     lien: '',
     repo: '',
-    skills: [],
+    skills: [], 
   });
-  const [preview, setPreview] = useState(null); // État pour l'aperçu de l'image
   const [editing, setEditing] = useState(null);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [imageFile, setImageFile] = useState(null); // État pour stocker le fichier d'image
+  const [previewUrl, setPreviewUrl] = useState(''); // État pour l'URL de prévisualisation
   const router = useRouter();
 
   useEffect(() => {
-    const userId = Cookies.get('userId');
+    const userId = Cookies.get('userId'); 
     if (!userId) {
       router.push('/auth/signin');
     } else {
-      setIsAuthenticated(true);
-      fetchProjects();
+      setIsAuthenticated(true); 
+      fetchProjects(); 
       fetchSkills();
     }
-  }, [router]);
+  }, [router]); 
 
   const fetchProjects = async () => {
     try {
@@ -61,56 +62,55 @@ const AdminProjects = () => {
     });
   };
 
-  const handleFileChange = (e) => {
+  // Gestion de l'ajout d'image
+  const handleImageChange = (e) => {
     const file = e.target.files[0];
-    setFormData({
-      ...formData,
-      img: file,
-    });
-    setPreview(file ? URL.createObjectURL(file) : null); // Mise à jour de l'aperçu de l'image
+    if (file) {
+      setImageFile(file);
+      setPreviewUrl(URL.createObjectURL(file)); // Créez une URL pour la prévisualisation
+    }
   };
 
   const handleCheckboxChange = (skillId) => {
     setFormData((prevFormData) => {
       const skills = [...prevFormData.skills];
-      return skills.includes(skillId)
-        ? { ...prevFormData, skills: skills.filter((id) => id !== skillId) }
-        : { ...prevFormData, skills: [...skills, skillId] };
+      if (skills.includes(skillId)) {
+        return { ...prevFormData, skills: skills.filter((id) => id !== skillId) };
+      } else {
+        return { ...prevFormData, skills: [...skills, skillId] };
+      }
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formDataToSubmit = new FormData();
-    formDataToSubmit.append('name', formData.name);
-    formDataToSubmit.append('description', formData.description);
-    formDataToSubmit.append('lien', formData.lien);
-    formDataToSubmit.append('repo', formData.repo);
-    formDataToSubmit.append('skills', JSON.stringify(formData.skills));
-
-    if (formData.img) {
-      formDataToSubmit.append('img', formData.img);
-    }
-
     try {
+      const data = new FormData();
+      data.append("name", formData.name);
+      data.append("description", formData.description);
+      data.append("lien", formData.lien);
+      data.append("repo", formData.repo);
+      data.append("skills", JSON.stringify(formData.skills)); // Convertir skills en string pour l'envoyer
+      
+      if (imageFile) {
+        data.append("img", imageFile); // Ajouter le fichier d'image si sélectionné
+      }
+      
       if (editing) {
-        await axios.put(`/api/projects/${editing}`, formDataToSubmit, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        });
+        await axios.put(`/api/projects/${editing}`, data);
       } else {
-        await axios.post('/api/projects', formDataToSubmit, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        });
+        await axios.post('/api/projects', data);
       }
       setFormData({
         name: '',
-        img: null,
+        img: '',
         description: '',
         lien: '',
         repo: '',
-        skills: [],
+        skills: [], 
       });
-      setPreview(null); // Réinitialisation de l'aperçu
+      setImageFile(null); // Réinitialiser l'image
+      setPreviewUrl(''); // Réinitialiser la prévisualisation
       setEditing(null);
       fetchProjects();
     } catch (error) {
@@ -121,10 +121,8 @@ const AdminProjects = () => {
   const handleEdit = (project) => {
     setFormData({
       ...project,
-      skills: project.skills.map((skill) => skill.id),
-      img: null,
+      skills: project.skills.map(skill => skill.id)
     });
-    setPreview(null); // Reset de l'aperçu lors de l'édition
     setEditing(project.id);
   };
 
@@ -143,7 +141,7 @@ const AdminProjects = () => {
 
   return (
     <>
-      <nav className="navadmin">
+      <nav className='navadmin'>
         <Link href="/admin">Retour à l'Admin</Link>
       </nav>
       <div className="admin-projects">
@@ -158,18 +156,25 @@ const AdminProjects = () => {
             placeholder="Nom du projet"
             required
           />
+
           <input
             className="input-img"
             type="file"
             name="img"
-            onChange={handleFileChange}
+            onChange={handleImageChange}
             accept="image/*"
           />
-          {preview && ( // Affichage de l'aperçu de l'image
-            <div className="image-preview">
-              <Image src={preview} alt="Aperçu de l'image" width={100} height={100} />
-            </div>
+          
+          {previewUrl && (
+            <Image
+              src={previewUrl}
+              alt="Aperçu de l'image"
+              width={100}
+              height={100}
+              className="image-preview"
+            />
           )}
+
           <textarea
             className="input-description"
             name="description"
@@ -196,14 +201,10 @@ const AdminProjects = () => {
             placeholder="Lien du dépôt"
             required
           />
-          <label className="skills-label" htmlFor="skills">
-            Compétences liées:
-          </label>
+
+          <label className="skills-label" htmlFor="skills">Compétences liées:</label>
           <div className="custom-dropdown">
-            <div
-              className="dropdown-toggle"
-              onClick={() => setDropdownOpen(!dropdownOpen)}
-            >
+            <div className="dropdown-toggle" onClick={() => setDropdownOpen(!dropdownOpen)}>
               Sélectionner des compétences
             </div>
             {dropdownOpen && (
@@ -221,10 +222,53 @@ const AdminProjects = () => {
               </div>
             )}
           </div>
-          <button className="submit-button" type="submit">
-            {editing ? 'Modifier' : 'Ajouter'}
-          </button>
+
+          <button className="submit-button" type="submit">{editing ? 'Modifier' : 'Ajouter'}</button>
         </form>
+
+        <h2 className="project-list-title">Liste des Projets</h2>
+        <ul className="project-list">
+          {projects.map((project) => (
+            <li className="project-card" key={project.id}>
+              <h3 className="project-name">{project.name}</h3>
+              <Image 
+                className="project-img" 
+                src={project.img} 
+                alt={project.name} 
+                width={100} 
+                height={100} 
+              />
+              <p className="project-description">{project.description}</p>
+              <h4 className="skills-title">Compétences:</h4>
+              <ul className="skills-list">
+                {project.skills.length > 0 ? (
+                  project.skills.map((skill) => (
+                    <li className="skill-item" key={skill.id}>
+                      <Image 
+                        className="skill-img" 
+                        src={skill.image} 
+                        alt={skill.name} 
+                        width={30} 
+                        height={30} 
+                      />
+                      {skill.name}
+                    </li>
+                  ))
+                ) : (
+                  <li className="no-skills">Aucune compétence associée.</li>
+                )}
+              </ul>
+              <p className="projectlink">
+                <strong>Lien:</strong> <a href={project.lien} target="_blank" rel="noopener noreferrer">{project.lien}</a>
+              </p>
+              <p className="project-repo">
+                <strong>Repo:</strong> <a href={project.repo} target="_blank" rel="noopener noreferrer">{project.repo}</a>
+              </p>
+              <button className="edit-button" onClick={() => handleEdit(project)}>Modifier</button>
+              <button className="delete-button" onClick={() => handleDelete(project.id)}>Supprimer</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
